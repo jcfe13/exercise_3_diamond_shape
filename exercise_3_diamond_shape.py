4c2aa3ae920977e0e3bd81a40d8a155d6bec2ceb
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 17:27:25 +0800

    If the number is even, ask the user to provide odd integer

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -1,3 +1,5 @@
 # 1. Ask the user to input an odd integer
 n = int(input("Enter an odd integer: "))
 # 2. Condition to check if the number entered is odd
+if n % 2 == 0:
+    print("Please provide an odd integer.")
