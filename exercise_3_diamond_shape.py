5f835f64019a1508a1acbcc3fa361fb62d6eb518
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 17:30:14 +0800

    New line

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -1,10 +1,12 @@
 # 1. Ask the user to input an odd integer
 n = int(input("Enter an odd integer: "))
+
 # 2. Condition to check if the number entered is odd
 if n % 2 == 0:
     print("Please provide an odd integer.")
 else:
     for i in range(n // 2 + 1):  # Looping for the upper part
         print(" " * (n // 2 - i) + "*" * (2 * i + 1))
+        
     for i in range(n // 2 - 1, -1, -1): # Looping for the lower part
         print(" " * (n // 2 - i) + "*" * (2 * i + 1))
