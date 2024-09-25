9dd1c776c7b624f9873effebf42775e227957e91
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 17:30:45 +0800

    Done

--- exercise_3_diamond_shape.py
+++ exercise_3_diamond_shape.py
@@ -7,6 +7,7 @@ if n % 2 == 0:
 else:
     for i in range(n // 2 + 1):  # Looping for the upper part
         print(" " * (n // 2 - i) + "*" * (2 * i + 1))
-        
+
     for i in range(n // 2 - 1, -1, -1): # Looping for the lower part
         print(" " * (n // 2 - i) + "*" * (2 * i + 1))
+        
